45283a5f4cf70f5759013d01e3685c1fdb3b0d4c
Joel <joel.bang@eformworks.com>
2025-03-18 10:08:04 +0900

    fix: 깜빡임 방지 스타일 수정
    - 스타일 요소에 ID 추가
    - DOMContentLoaded 이벤트에서 스타일 제거 시 ID 사용

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -22,12 +22,16 @@ export const onRenderBody = ({ setHeadComponents }) => {
             // 깜빡임 방지를 위한 스타일 추가
             const style = document.createElement('style');
             style.innerHTML = 'html { visibility: hidden; }';
+            style.id = 'gatsby-theme-style-loader';
             document.head.appendChild(style);
 
             // DOM이 로드되면 테마 설정 및 깜빡임 방지 스타일 제거
             document.addEventListener('DOMContentLoaded', function() {
               setTheme();
-              document.querySelector('style').remove();
+              const themeStyle = document.getElementById('gatsby-theme-style-loader');
+              if (themeStyle) {
+                themeStyle.remove();
+              }
               document.documentElement.style.visibility = 'visible';
             });
           })();
